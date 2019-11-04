c0d590eed0743675ec4016668c71973eae4913ae
Jignesh <32303221+makwanji@users.noreply.github.com>
2019-11-04 12:23:04 +0800

    Add files via upload

---
+++ shell/ebsEnv.sh
@@ -0,0 +1,11 @@
+#!/bin/ksh
+# set -x
+# -------------------------------------------------------------------------------
+#  SCRIPT          : ebsEnv.sh
+#  Application     : Oracle EBS DBA
+#  Fonction        : set the application variable
+#  History
+#  --------
+#  Date                Author          		 Update
+#  11/1/2019           Jig                   Creation
+# -------------------------------------------------------------------------------
