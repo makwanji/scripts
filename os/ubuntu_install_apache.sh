db16bd3d6e2e68f83ce9c0d0c37df955942dcd81
Jig <makwana.jignesh@gmail.com>
2019-11-13 11:58:31 +0800

    new file

---
+++ os/ubuntu_install_apache.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+# set -x
+# -------------------------------------------------------------------------------
+#  SCRIPT          : ubuntu_install_apache.sh
+#  Application     : Install Apache in Ubuntu server
+#  Fonction        : call script to instll apache in Ubuntu folder
+#  History
+#  --------
+#  Date                Author          		 Update
+#  11/13/2019          Jig                   Creation
+# -------------------------------------------------------------------------------
+
+sudo yum update -y
