bbd3efeaf4d94ea3abd6d9fd6c929817b1e9be7c
unknown <makwana.jignesh@gmail.com>
2019-11-16 17:14:20 +0800

    new script

---
+++ os/mysql_backup_database.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh
+# set -x
+# -------------------------------------------------------------------------------
+#  SCRIPT          : mysql_backup_database.sh
+#  Application     : MYSQL Database script to take backup
+#  Fonction        : MYSQL Database script to take backup
+#  History
+#  --------
+#  Date                Author          		 Update
+#  11/16/2019          Jig                   Creation
+# -------------------------------------------------------------------------------
+
+# Update Server
