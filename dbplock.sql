9c3573babcf14b9a706443918b8aeb81d03fcc6e
Jignesh <32303221+makwanji@users.noreply.github.com>
2020-06-11 19:37:22 +0800

    Create dbplock.sql

---
+++ dbplock.sql
@@ -0,0 +1,11 @@
+SET PAGESIZE 60
+SET LINESIZE 300
+
+COLUMN owner FORMAT A20
+COLUMN username FORMAT A20
+COLUMN object_owner FORMAT A20
+COLUMN object_name FORMAT A30
+COLUMN locked_mode FORMAT A35
+
+SELECT b.inst_id, b.session_id AS sid, NVL(b.oracle_username, '(oracle)') AS username, a.owner AS object_owner, a.object_name, Decode(b.locked_mode, 0, 'None', 1, 'Null (NULL)', 2, 'Row-S (SS)', 3, 'Row-X (SX)', 4, 'Share (S)', 5, 'S/Row-X (SSX)', 6, 'Exclusive (X)', b.locked_mode) locked_mode, b.os_user_name FROM   dba_objects a, gv$locked_object b WHERE  a.object_id = b.object_id ORDER BY 1, 2, 3, 4; 
+
